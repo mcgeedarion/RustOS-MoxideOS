49689c5e6ca1e8f7a7d9942a9631d0ec3d452c55
mcgeedarion <70579384+mcgeedarion@users.noreply.github.com>
2026-05-13 20:47:11 -0400

    fmt: format and optimise /src batch 2/10 (console, debug, drivers)

--- src/console/console.rs
+++ src/console/console.rs
@@ -1,17 +1,1 @@
-//! Kernel console (writes to serial + VGA if available).
-extern crate alloc;
-use alloc::string::String;
-
-pub fn print(s: &str) {
-    #[cfg(target_arch="x86_64")]
-    for b in s.bytes() { crate::arch::x86_64::serial::write_byte(b); }
-}
-
-#[macro_export]
-macro_rules! kprintln {
-    ($($arg:tt)*) => {{
-        use alloc::format;
-        crate::console::console::print(&format!($($arg)*));
-        crate::console::console::print("\n");
-    }}
-}
+$(python3 -c "import json; d=json.load(open('/tmp/batch_files_1.json')); [print(f['content']) for f in d if f['path']=='src/console/console.rs']")
